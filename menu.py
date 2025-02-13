6b2257b78f47c084a22befb54bb01dab64ebfdb8
Adrian Con <adriancongarcia10@gmail.com>
2025-02-13 16:59:01 +0100

    Modified text.

--- menu.py
+++ menu.py
@@ -2,13 +2,13 @@ import streamlit as st
 import subprocess
 
 st.sidebar.title("Navigation")
-option = st.sidebar.selectbox("Go to:", ["Stock Price App", "Other App"])
+option = st.sidebar.selectbox("Go to:", ["Stock Price App", "Bioinformatics DNA Count App"])
 
 if option == "Stock Price App":
-    st.write("### Welcome to Stock Price App")
+    st.write("### Welcome to the Stock Price App")
     # Run the first script
     subprocess.run(["python", "stock.py"])
 elif option == "Other App":
-    st.write("### Welcome to My Other App")
+    st.write("### Welcome to the Bioinformatics DNA Count App")
     # Run the second script
-    subprocess.run(["python", "other_project.py"])
+    subprocess.run(["python", "dna-app.py"])
